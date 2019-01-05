ee7e837e7c5c64217ddf0e4b173112176f15a5c1
Frank Quinn <fquinnjr@hotmail.com>
2019-01-04 21:16:42 -0500

    Persisting Data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,27 +10,40 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory
+        , in: .userDomainMask).first?.appendingPathComponent("Items.plist")//get first item in user's Document/ home directory
+  //   let defaults = UserDefaults.standard // only used for user default method
     
-    let defaults = UserDefaults.standard
+    //dataFilePath is a global constant up here
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
+        // Do any additional setup after loading the view, typically from a nib
         
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
         
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demorgogon"
-        itemArray.append(newItem3)
+        print(dataFilePath!)
+ //Following were just stub files to put in test data
+//        let newItem = Item()
+//        newItem.title = "Find Mike"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Buy Eggos"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Destroy Demorgogon"
+//        itemArray.append(newItem3)
+
+        loadItems()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+        // items is cast as an [Item] object array
+   /*     if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
+ */
     }
     //MARK TableView DataSource methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,23 +53,23 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-   //Let's replace the repetitious references to itemArray[indexPath.row] and replace it with a constant called 'item'
+        //Let's replace the repetitious references to itemArray[indexPath.row] and replace it with a constant called 'item'
         
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
         
-//        if item.done == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//
-//         cell.accessoryType = .none
-//    }
-//We have replaced the above lines using the Ternary operator of the form...
+        //        if item.done == true {
+        //            cell.accessoryType = .checkmark
+        //        } else {
+        //
+        //         cell.accessoryType = .none
+        //    }
+        //We have replaced the above lines using the Ternary operator of the form...
         // value = condition ? valueIfTrue : valueIfFalse like so...
         
-//     cell.accessoryType = item.done == true ? .checkmark : .none
- //Believe it or not we can even elimate == true to abbreviate like this...
+        //     cell.accessoryType = item.done == true ? .checkmark : .none
+        //Believe it or not we can even elimate == true to abbreviate like this...
         cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
@@ -66,15 +79,15 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Now we toggle checkmark boolean
-//        if itemArray[indexPath.row].done == false {
-//            itemArray[indexPath.row].done = true
-//        } else {
-//            itemArray[indexPath.row].done = false
-//        }
- //All of the above can be replaced with the ! NOT operator
+        //        if itemArray[indexPath.row].done == false {
+        //            itemArray[indexPath.row].done = true
+        //        } else {
+        //            itemArray[indexPath.row].done = false
+        //        }
+        //All of the above can be replaced with the ! NOT operator
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItems()
         
-        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     //MARK Add New items
@@ -87,23 +100,53 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
             //what will happen when user hits add
-            let newItem = Item()
+            let newItem = Item() //done property is false in Data Model item.swift
+            
             newItem.title = textField.text!
+            
             self.itemArray.append(newItem)
             
-            //Let's setup persistent storage using user defaults
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.saveItems()
             
-            self.tableView.reloadData()
+            //Let's setup persistent storage using user defaults
+            //Not using defaults anymore...
+       //     self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+           
         }
         alert.addTextField { (alertTextfield) in
             alertTextfield.placeholder = "Create new item"
-           textField = alertTextfield
+            textField = alertTextfield
         }
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
+    }
+//MARK - Model Manipulation methods
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+            
+        } catch {
+            print("Error encoding item Array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func loadItems() {
+        //We intentionally make try and optional so that we can use optional binding to unwrap it safely
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding Item array, \(error)")
+            }
+        }
     }
 }
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -7,8 +7,13 @@
 //
 
 import Foundation
+//Class must conform to the protocol Encodable so now the Item type is now able to encode itself into a plist or a JSON
 
-class Item {
+//Old way to enforce protocols seperately...class Item: Encodable, Decodable
+//New way just say Codable protocol which covers both Encodable and Decodable
+class Item: Codable
+{
+//for the above conforming to be possible, all the properties must be standard data types
     var title : String = ""
     var done : Bool = false
     
